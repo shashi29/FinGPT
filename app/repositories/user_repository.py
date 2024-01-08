ca150d399ceab55cc809b35528fda3452ffb7408
shashi29 <shashi.raj30@yahoo.com>
2024-01-08 17:37:50 +0000

    Add login changes

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,13 +1,10 @@
-# app/repositories/user_repository.py
-
-from typing import Any, Optional
-from psycopg2 import sql
+from typing import Any
+from datetime import datetime, timedelta
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy import text
 from app.repositories.base_repository import BaseRepository
 from app.models.user import User
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from datetime import datetime, timedelta
-from sqlalchemy import inspect, text
 
 SECRET_KEY = "test_token"
 ALGORITHM = "HS256"
@@ -25,9 +22,9 @@ class UserRepository(BaseRepository):
 
     def create_user(self, user: User) -> Any:
         query = text("""
-        INSERT INTO Users (name, email, password, client_number, customer_number)
-        VALUES (:name, :email, :password, :client_number, :customer_number)
-        RETURNING id, name, email, password, client_number, customer_number;
+            INSERT INTO Users (name, email, password, client_number, customer_number)
+            VALUES (:name, :email, :password, :client_number, :customer_number)
+            RETURNING id, name, email, password, client_number, customer_number;
         """)
 
         values = {
@@ -43,56 +40,54 @@ class UserRepository(BaseRepository):
         return user_instance
 
     def get_users(self) -> Any:
-        query = text(""" SELECT * FROM Users;""")
-
+        query = text("""SELECT * FROM Users;""")
         user_data_list = self.execute_query_all(query)
         user_dict = [User(**dict(zip(User.__annotations__, user_data))) for user_data in user_data_list]
         return user_dict
 
     def get_user(self, user_id: int) -> Any:
-        query = text(f""" SELECT * FROM Users WHERE id = {user_id};""")
-
-        user_data_tuple =  self.execute_query(query)
+        query = text("""SELECT * FROM Users WHERE id = :user_id;""")
+        values = {"user_id": user_id}
+        user_data_tuple = self.execute_query(query, values)
         user_instance = User(**dict(zip(User.__annotations__, user_data_tuple)))
         return user_instance
 
     def login_user(self, user_data) -> Any:
         query = text("""SELECT * FROM Users WHERE email = :email AND password = :password;""")
-        values = {
-                "email": user_data.email,
-                "password": user_data.password
-            }
-        user_data_tuple =  self.execute_query(query, values)
+        values = {"email": user_data.email, "password": user_data.password}
+        user_data_tuple = self.execute_query(query, values)
         user_instance = User(**dict(zip(User.__annotations__, user_data_tuple)))
         return user_instance
 
-    #To do : Fix this code
     def update_user(self, user_id: int, user: User) -> Any:
         query = text("""
             UPDATE Users
-            SET name = %s, email = %s, password = %s, client_number = %s, customer_number = %s
-            WHERE id = %s
+            SET name = :name, email = :email, password = :password,
+                client_number = :client_number, customer_number = :customer_number
+            WHERE id = :user_id
             RETURNING id, name, email, password, client_number, customer_number;
         """)
 
-        values = {"name": user.name, 
-                  "email": user.email, 
-                  "password" : user.password, 
-                  "client_number" : user.client_number, 
-                  "customer_care": user.customer_number, 
-                  "id":user_id}
+        values = {
+            "name": user.name, 
+            "email": user.email, 
+            "password": user.password, 
+            "client_number": user.client_number, 
+            "customer_number": user.customer_number,
+            "user_id": user_id
+        }
 
-        user_data_tuple =  self.execute_query(query, values)
+        user_data_tuple = self.execute_query(query, values)
         user_instance = User(**dict(zip(User.__annotations__, user_data_tuple)))
         return user_instance
 
-    #To do : Fix this code
     def delete_user(self, user_id: int) -> Any:
-        query = text(f"""
-            DELETE FROM Users WHERE id = {user_id}
+        query = text("""
+            DELETE FROM Users WHERE id = :user_id
             RETURNING id, name, email, password, client_number, customer_number;
         """)
 
+        values = {"user_id": user_id}
         user_data_tuple = self.execute_query(query, values)
         user_instance = User(**dict(zip(User.__annotations__, user_data_tuple)))
         return user_instance
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,30 +1,27 @@
-# app/routers/user_router.py
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
-from app.repositories.user_repository import UserRepository, create_access_token
-from app.models.user import User
-from app.database import get_database_connection
-from fastapi import FastAPI, Depends, HTTPException, status
+from datetime import timedelta
 from fastapi.responses import JSONResponse
-from datetime import datetime, timedelta
-from app.exceptions import UserNotFoundException, EmailAlreadyInUseException, InternalServerErrorException
 from fastapi.security import OAuth2PasswordBearer
+from app.models.user import User
+from app.repositories.user_repository import UserRepository, create_access_token
+from app.exceptions import UserNotFoundException, EmailAlreadyInUseException, InternalServerErrorException
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
 ACCESS_TOKEN_EXPIRE_MINUTES = 1440  # 24 hours
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
 user_repository = UserRepository()
 
 @router.post("/", response_model=User)
 async def create_user(user: User):
-    # try:
-    created_user = user_repository.create_user(user)
-    return created_user
-    # except Exception as e:
-    #     raise InternalServerErrorException from e
+    try:
+        created_user = user_repository.create_user(user)
+        return created_user
+    except EmailAlreadyInUseException:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email is already in use")
+    except InternalServerErrorException as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @router.get("/", response_model=List[User])
 async def get_users():
@@ -33,22 +30,27 @@ async def get_users():
 
 @router.get("/{user_id}", response_model=User)
 async def get_user(user_id: int):
-    user = user_repository.get_user(user_id)
-    if not user:
-        raise UserNotFoundException
-    return user
+    try:
+        user = user_repository.get_user(user_id)
+        if not user:
+            raise UserNotFoundException
+        return user
+    except UserNotFoundException:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 @router.put("/{user_id}", response_model=User)
 async def update_user(user_id: int, user: User):
-#    try:
-    updated_user = user_repository.update_user(user_id, user)
-    if not updated_user:
-        raise UserNotFoundException
-    return updated_user
-    # except IntegrityError:
-    #     raise EmailAlreadyInUseException
-    # except Exception as e:
-    #     raise InternalServerErrorException from e
+    try:
+        updated_user = user_repository.update_user(user_id, user)
+        if not updated_user:
+            raise UserNotFoundException
+        return updated_user
+    except EmailAlreadyInUseException:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email is already in use")
+    except UserNotFoundException:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except InternalServerErrorException as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @router.delete("/{user_id}", response_model=dict)
 async def delete_user(user_id: int):
@@ -58,30 +60,32 @@ async def delete_user(user_id: int):
             raise UserNotFoundException
         response_data = {"status_code": 200, "detail": "User deleted successfully"}
         return response_data
-    except Exception as e:
-        raise InternalServerErrorException from e
+    except UserNotFoundException:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    except InternalServerErrorException as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 @router.post("/login", response_model=dict)
 def login(user_data: User):
-    # try:
-    #cur = db.connect()
-    #cur.execute("SELECT * FROM Users WHERE email = %s AND password = %s", {"email":user_data.email, "password":user_data.password})
-    user = user_repository.login_user(user_data)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    try:
+        user = user_repository.login_user(user_data)
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
-    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user_data.email}, expires_delta=expires_delta)
+        expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": user_data.email}, expires_delta=expires_delta)
 
-    response_data = {
-        "access_token": access_token,
-        "token_type": "bearer",
-        "user_id": user.id,
-        "user_name": user.name,
-        "email": user.email,
-        # Add other user details as needed
-    }
+        response_data = {
+            "access_token": access_token,
+            "token_type": "bearer",
+            "user_id": user.id,
+            "user_name": user.name,
+            "email": user.email,
+            # Add other user details as needed
+        }
 
-    return JSONResponse(content=response_data)
-    # except Exception as e:
-    #     raise InternalServerErrorException from e
+        return JSONResponse(content=response_data)
+    except HTTPException as e:
+        raise e
+    except InternalServerErrorException as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
