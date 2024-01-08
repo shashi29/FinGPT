98f9219441a08442e840b9dc7440f8f05f21cae9
shashi29 <shashi.raj30@yahoo.com>
2024-01-08 17:52:09 +0000

    Add board changes

--- app/repositories/board_repository.py
+++ app/repositories/board_repository.py
@@ -1,7 +1,7 @@
 # app/repositories/board_repository.py
 
-from typing import Any, Optional
-from psycopg2 import sql
+from typing import Any
+from sqlalchemy import text
 from app.repositories.base_repository import BaseRepository
 from app.models.board import Board
 
@@ -10,59 +10,72 @@ class BoardRepository(BaseRepository):
         super().__init__('Boards')
 
     def create_board(self, board: Board) -> Any:
-        query = sql.SQL("""
+        query = text("""
             INSERT INTO Boards (user_id, name, created_at, client_number, customer_number)
-            VALUES (%s, %s, %s, %s, %s)
+            VALUES (:user_id, :name, :created_at, :client_number, :customer_number)
             RETURNING id, user_id, name, created_at, client_number, customer_number;
-        """).format(table_name=sql.Identifier(self.table_name))
+        """)
 
-        values = (board.user_id, board.name, board.created_at, board.client_number, board.customer_number)
+        values = {
+            "user_id": board.user_id,
+            "name": board.name,
+            "created_at": board.created_at,
+            "client_number": board.client_number,
+            "customer_number": board.customer_number
+        }
 
         board_data_tuple = self.execute_query(query, values)
         board_instance = Board(**dict(zip(Board.__annotations__, board_data_tuple)))
         return board_instance
 
     def get_boards(self) -> Any:
-        query = sql.SQL("""
+        query = text("""
             SELECT * FROM Boards;
-        """).format(table_name=sql.Identifier(self.table_name))
+        """)
 
         board_data_list = self.execute_query_all(query)
         board_dict = [Board(**dict(zip(Board.__annotations__, board_data))) for board_data in board_data_list]
         return board_dict
 
     def get_board(self, board_id: int) -> Any:
-        query = sql.SQL("""
-            SELECT * FROM Boards WHERE id = %s;
-        """).format(table_name=sql.Identifier(self.table_name))
+        query = text("""
+            SELECT * FROM Boards WHERE id = :board_id;
+        """)
 
-        values = (board_id,)
+        values = {"board_id": board_id}
 
         board_data_tuple = self.execute_query(query, values)
         board_instance = Board(**dict(zip(Board.__annotations__, board_data_tuple)))
         return board_instance
 
     def update_board(self, board_id: int, board: Board) -> Any:
-        query = sql.SQL("""
+        query = text("""
             UPDATE Boards
-            SET name = %s, created_at = %s, client_number = %s, customer_number = %s
-            WHERE id = %s
+            SET name = :name, created_at = :created_at,
+                client_number = :client_number, customer_number = :customer_number
+            WHERE id = :board_id
             RETURNING id, user_id, name, created_at, client_number, customer_number;
-        """).format(table_name=sql.Identifier(self.table_name))
+        """)
 
-        values = (board.name, board.created_at, board.client_number, board.customer_number, board_id)
-        
+        values = {
+            "name": board.name,
+            "created_at": board.created_at,
+            "client_number": board.client_number,
+            "customer_number": board.customer_number,
+            "board_id": board_id
+        }
+
         board_data_tuple = self.execute_query(query, values)
         board_instance = Board(**dict(zip(Board.__annotations__, board_data_tuple)))
         return board_instance
 
     def delete_board(self, board_id: int) -> Any:
-        query = sql.SQL("""
-            DELETE FROM Boards WHERE id = %s
+        query = text("""
+            DELETE FROM Boards WHERE id = :board_id
             RETURNING id, user_id, name, created_at, client_number, customer_number;
-        """).format(table_name=sql.Identifier(self.table_name))
+        """)
 
-        values = (board_id,)
+        values = {"board_id": board_id}
 
         board_data_tuple = self.execute_query(query, values)
         board_instance = Board(**dict(zip(Board.__annotations__, board_data_tuple)))
